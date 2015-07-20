13ca42ee83feb8b96bd48fb66157d0fd5f4ef2d1
Zuguang Gu <jokergoo@gmail.com>
2015-07-21 00:27:38 +0200

    Update movie.R

--- example/movie.R
+++ example/movie.R
@@ -1,1 +1,8 @@
+## Premiers in Poland of movies from 2014
+## <br>
+## Example is provided by <a href="https://github.com/pbiecek">Przemyslaw Biecek</a>
+## <br>
+## <a href="https://gist.github.com/pbiecek/9c8263e9949e46b964db">https://gist.github.com/pbiecek/9c8263e9949e46b964db</a>
+##
+
 source("https://gist.githubusercontent.com/pbiecek/9c8263e9949e46b964db/raw/1f765215a66e2902d9c4b83915951f6f42731d02/circ.R")
